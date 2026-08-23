297e1498a58a0266decc667c08686b5975dab406
rishavvrajj <rishav5raj3@gmail.com>
2026-08-24 05:25:18 +0530

    feat: add new chart components and update main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import { Button } from '@/components/ui/button'
-import { CirclePlus } from 'lucide-react'
-import { cn } from '@/lib/utils'
 
-export default function page({
-  disabled,
-  isRounded,
-} : {
-  disabled: boolean;
-  isRounded: boolean;
-}) {
+import { AppBarChart } from '@/components/AppBarChart';
+import { AppAreaChart } from '@/components/AppAreaChart';
+import { AppPieChart } from '@/components/AppPieChart';
+
+export default function page() {
   return (
-    <div className='flex items-center justify-center min-h-screen'>
-        <Button className="cursor-pointer"> 
-          <CirclePlus /> 
-          <span className={cn('text-sm', disabled ? "text-red" : "text-blue", isRounded && "rounded-full", "p-4")}>Click Me</span>
-        </Button>
+    <div className='grid grid-cols-1 lg:grid-cols-4 2xl:grid-cols-4 gap-4 p-4'>
+      <div className='bg-card rounded-lg p-4'>
+        <AppPieChart />
+      </div>
+      <div className='bg-card rounded-lg p-4 lg:col-span-2'>
+        <AppBarChart />
+      </div>
+      <div className='bg-card rounded-lg p-4'>Test</div>
+      <div className='bg-card rounded-lg p-4 lg:col-span-2'>
+        <AppAreaChart />
+      </div>
+      <div className='bg-card rounded-lg p-4'>Test</div>
+      <div className='bg-card rounded-lg p-4'>Test</div>
     </div>
   )
-}
-
+};
